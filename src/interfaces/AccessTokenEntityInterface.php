a9abbbfd0d0d8e19e89652bdc13e8eb7708993d6
ZhangYanJiong <zhangyanjiong@163.com>
2018-12-21 15:17:56 +0800

    initial commit

---
+++ src/interfaces/AccessTokenEntityInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link https://github.com/devzyj/php-oauth2-server
+ * @copyright Copyright (c) 2018 Zhang Yan Jiong
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+namespace devzyj\oauth2\server\interfaces;
+
+/**
+ * 访问令牌实体接口。
+ * 
+ * @author ZhangYanJiong <zhangyanjiong@163.com>
+ * @since 1.0
+ */
+interface AccessTokenEntityInterface extends BaseTokenCodeEntityInterface
+{
+    
+}
